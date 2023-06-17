ee95e9b8186feb4efe44f9ce68e97ed87f3809d7
SunilCz <sunil.chaudhary7281@gmai.com>
2023-06-17 09:08:40 +0545

    add api and car details props

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
-import { CustomFilter, Hero, SearchBar } from '@/components'
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { fetchCars } from '@/utils';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length<1 || !allCars;
   return (
   <main className='overflow-hidden '>
     <Hero/>
@@ -21,7 +24,23 @@ export default function Home() {
 
         </div>
       </div>
+      {!isDataEmpty ? (
+      <section>
+       <div className='home__cars-wrapper'>
+        {allCars?.map((car) => (
+        <CarCard car={car}/> 
+        ))}
 
+       </div>
+      </section>):
+       <div className='home__error-container'>
+        <h2 className='text-black-xl font-bold'>
+          No cars found
+        </h2>
+        <p>{allCars?.message}</p>
+       </div>
+
+      }
 
     </div>
 
--- types/index.ts
+++ types/index.ts
@@ -6,9 +6,27 @@ export interface CustomButtonProps{
     handleClick?:
     MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit" | "reset";
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
 }
 
 export interface SearchManuFacturerProps{
     manufacturer: string;
     setManuFacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps{
+city_mpg:number;
+class:string;
+combination_mpg:string;
+cylinders:number;
+displacement:number;
+drive:string;
+fuel_type:string;
+highway_mpg:number;
+make:string;
+model:string;
+transmission:string;
+year:number;
 }
